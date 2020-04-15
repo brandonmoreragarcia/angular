6c1881a80f50d8fc0a7c082ca77fae7c8c9a7721
brandon <brandonmoreragarcia@gmail.com>
2020-04-15 08:58:38 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
